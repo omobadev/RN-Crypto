1ed6f28dbed5d34fe1f43eeede48235c49534eb5
Artem Moshnin <62706319+Artem711@users.noreply.github.com>
2020-08-05 00:34:37 +0200

    s

--- App.tsx
+++ App.tsx
@@ -68,6 +68,7 @@ const App: React.FC<PropsType> = (props) => {
                 function: () => console.log(),
               }}
             />
+            
           </Stack.Navigator>
         ) : (
           <Auth />
